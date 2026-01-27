26d7812f205f6dbb517fc592aa4245d9784cd5b5
Tunmise <tunmistom@gmail.com>
2026-01-27 15:40:55 +0100

    Update_1_0_0

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 function Home({ goToCalc }) {
   return (
     <div className="home">
-      <h1>Calculator</h1>
+      <h1>Scientific Calculator</h1>
       <p>Fast • Keyboard Enabled</p>
       <button onClick={goToCalc}>Open Calculator</button>
     </div>
